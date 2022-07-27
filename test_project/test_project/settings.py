a1c4379ebd44a4e7363e63862d7e93635458a614
Narjes Sh <narjes1379sh@gmail.com>
2022-07-27 14:26:55 +0430

    add apps

--- test_project/test_project/settings.py
+++ test_project/test_project/settings.py
@@ -37,7 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+    'cart',
+    'comment',
+    'introduction',
+    'payment',
+    'product',
+    'user',
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -72,13 +78,15 @@ WSGI_APPLICATION = 'test_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+'''
+
 
 
 # Password validation
